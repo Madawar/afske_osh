5ceed01b873a6aba9c505141eea57747d5289e51
Dennis Wanyoike Kuria <dwanyoike@codedcell.com>
2021-11-04 10:39:34 +0300

    Fix Photo Upload when Incident is not saved

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -2,7 +2,9 @@
     <label class="label">
         <span class="label-text block text-xs font-semibold text-gray-600 uppercase">{{ $label }}</span>
     </label>
-    <textarea   {{ $attributes->class(['textarea h-24 textarea textarea-bordered','input-error'=>$errors->has($name)])->merge(['class' => '']) }}  name="{{ $name }}" placeholder="{{ $placeholder }}">@if (isset($value)){{ $value }} @endif</textarea>
+    <textarea
+        {{ $attributes->class(['textarea h-24 textarea textarea-bordered', 'input-error' => $errors->has($name)])->merge(['class' => '']) }}
+        name="{{ $name }}" placeholder="{{ $placeholder }}">@if (isset($value)){{ $value }} @endif</textarea>
     @error($name)
         <label class="label">
             <span class="label-text-alt">{{ $message }}</span>
